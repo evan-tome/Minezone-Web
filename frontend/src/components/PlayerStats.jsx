61ab232c09183fbc043a5a40cf0e54c0500ad682
evant <evstike@gmail.com>
2025-09-20 12:43:05 -0400

    Started work on player stats layout

--- frontend/src/components/PlayerStats.jsx
+++ frontend/src/components/PlayerStats.jsx
@@ -4,12 +4,49 @@ function PlayerStats({ player }) {
     const headUrl = `https://minotar.net/helm/${player.UUID}/128.png`;
 
     return (
-        <div className="player-stats">
-            <div className="stats-header">
+        <div className="stats-card">
+            <div className="profile">
                 <img className="player-head" src={headUrl} alt="Head"/>
-                <h2>{player.LastPlayerName}'s Stats</h2>
+                <div className="username">{player.LastPlayerName}</div>
             </div>
-            <div className="overall-stats">
+
+            <div className="title">
+                <div className="yellow">Level: {player.Level}</div>
+                <div className="yellow">Progress: {player.Exp}/2500</div>
+                <div className="progress-bar">
+                    <div className="progress" style={{ width: `${(player.Exp / 2500) * 100}%` }}></div>
+                </div>
+            </div>
+
+            <div className="stats">
+                <div className="stat-box green">
+                    Wins<br />
+                    <span>{player.Wins}</span>
+                </div>
+                <div className="stat-box red">
+                    Losses<br />
+                    <span>{player.Losses}</span>
+                </div>
+                <div className="stat-box orange">
+                    WLR<br />
+                    <span>{player.Losses > 0 ? (player.Wins / player.Losses).toFixed(3) : player.Wins}</span>
+                </div>
+                <div className="stat-box green">
+                    Kills<br />
+                    <span>{player.Kills}</span>
+                </div>
+                <div className="stat-box red">
+                    Deaths<br />
+                    <span>{player.Deaths}</span>
+                </div>
+                <div className="stat-box orange">
+                    KDR<br />
+                    <span>{player.Deaths > 0 ? (player.Kills / player.Deaths).toFixed(3) : player.Kills}</span>
+                </div>
+            </div>
+
+
+            {/* <div className="overall-stats">
                 <h3>Overall</h3>
                 <ul>
                     <li>Level: {player.Level}</li>
@@ -37,7 +74,7 @@ function PlayerStats({ player }) {
                 <ul>
                     <li>Lifetime Caught: {player.TotalCaught}</li>
                 </ul>
-            </div>
+            </div> */}
         </div>
     );
 }
